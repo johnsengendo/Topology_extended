385303ff594ddb18632acf9b3367a13330f677bb
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2025-01-28 16:42:28 +0100

    Update video_streaming.py

--- server/video_streaming.py
+++ server/video_streaming.py
@@ -43,7 +43,7 @@ def main():
     Main function to handle video streaming with optional packet capture.
     """
     input_file = "video/Deadpool.mp4"
-    loops_number = -1  # Stream the video once, without looping
+    loops_number = 0  # Stream the video once, without looping
     capture_traffic = True
 
     pids = []
@@ -56,7 +56,7 @@ def main():
 
     ffmpeg_command = [
         "ffmpeg", "-loglevel", "info", "-stats", "-re", "-stream_loop", str(loops_number), "-i", input_file,
-        "-t", "600", "-c:v", "copy", "-c:a", "aac", "-ar", "44100", "-ac", "1",
+        "-t", "60", "-c:v", "copy", "-c:a", "aac", "-ar", "44100", "-ac", "1",
         "-f", "flv", "rtmp://localhost:1935/live/video.flv"
     ]
     subprocess.run(ffmpeg_command)  # Running ffmpeg command to stream video
